f1e9fbdc60b44d8cf8dff91ba26e119c76817997
Gonzalo Correa <gcorrea.q@gmail.com>
2023-05-01 21:50:38 -0700

    Simpler formatter

--- src/geanpy/logger.py
+++ src/geanpy/logger.py
@@ -8,7 +8,7 @@ def setup_logger(logging_level: int | str | None = None) -> logging.Logger:
     logger.setLevel(logging_level)
     ch = logging.StreamHandler()
     ch.setLevel(logging_level)
-    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+    formatter = logging.Formatter("%(levelname)s | %(message)s")
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
